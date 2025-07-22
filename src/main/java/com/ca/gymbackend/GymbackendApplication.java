69b7a7724d6fa45a4536fd9ec6ae9821b70bf27e
ssoi <rainbowfox1@naver.com>
2025-07-22 15:11:36 +0900

    첫시작

---
+++ src/main/java/com/ca/gymbackend/GymbackendApplication.java
@@ -0,0 +1,13 @@
+package com.ca.gymbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ca/gymbackend/GymbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca.gymbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
