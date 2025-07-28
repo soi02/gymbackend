8035bc857c555a8913bb3a28dcb734fdbffc8b47
jinnycalm <jinnycalm@gmail.com>
2025-07-28 20:09:22 +0900

    흑흑

--- src/main/java/com/ca/gymbackend/routine/mapper/RoutineSqlMapper.java
+++ src/main/java/com/ca/gymbackend/routine/mapper/RoutineSqlMapper.java
@@ -1,6 +1,12 @@
 package com.ca.gymbackend.routine.mapper;
 
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ca.gymbackend.routine.dto.WorkoutElementDto;
+
+@Mapper
 public interface RoutineSqlMapper {
-
+    public List<WorkoutElementDto> findAllWorkout();
 }
--- src/main/java/com/ca/gymbackend/routine/service/RoutineService.java
+++ src/main/java/com/ca/gymbackend/routine/service/RoutineService.java
@@ -1,5 +1,21 @@
 package com.ca.gymbackend.routine.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ca.gymbackend.routine.dto.WorkoutElementDto;
+import com.ca.gymbackend.routine.mapper.RoutineSqlMapper;
+
+@Service
 public class RoutineService {
 
+    @Autowired
+    private RoutineSqlMapper routineSqlMapper;
+
+    
+    public  List<WorkoutElementDto> getArticleList() {
+        return routineSqlMapper.findAllWorkout();
+    }
 }
