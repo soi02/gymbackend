d9e9cc6cf0f4e6e943859c7d0c0e220f2c2457b7
MoonHyeok <albehap@gmail.com>
2025-08-01 11:05:43 +0900

    구조 보완

---
+++ src/main/java/com/ca/gymbackend/market/dto/MarketCommentOnArticleDto.java
@@ -0,0 +1,15 @@
+package com.ca.gymbackend.market.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class MarketCommentOnArticleDto {
+    private int id;
+    private int articleId;
+    private int marketUserId;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
