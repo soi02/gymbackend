94695b77cc4913e1e2b75f7a81fdf2df93798eab
MoonHyeok <albehap@gmail.com>
2025-08-04 20:45:32 +0900

    백엔드 로직 기초 기반

--- src/main/java/com/ca/gymbackend/market/mapper/MarketMapper.java
+++ src/main/java/com/ca/gymbackend/market/mapper/MarketMapper.java
@@ -1,8 +1,50 @@
 package com.ca.gymbackend.market.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.ca.gymbackend.market.dto.MarketArticleDto;
+import com.ca.gymbackend.market.dto.MarketCommentOnArticleDto;
+import com.ca.gymbackend.market.dto.MarketDealedLogDto;
+import com.ca.gymbackend.market.dto.MarketProductInterestedLogDto;
+import com.ca.gymbackend.market.dto.MarketReviewOnUserDto;
+import com.ca.gymbackend.market.dto.MarketUserInfoDto;
 
 @Mapper
 public interface MarketMapper {
-
+    
+    // [ 테이블 당 기본 crud ]
+    
+    public MarketUserInfoDto selectMarketUserInfo(@Param("userId") Integer userId);
+    
+    public void insertMarketArticle(@Param("marketArticleDto") MarketArticleDto marketArticleDto);
+    public MarketArticleDto selectMarketArticle(@Param("id") Integer id);
+    public void updateMarketArticle(@Param("marketArticleDto") MarketArticleDto marketArticleDto);
+    // 장터 게시글 내용 정보 바꾸기, 장터 게시글 판매 여부 바꾸기 분할 예정
+    public void deleteMarketArticle(@Param("id") Integer id);
+    
+    public void insertMarketCommentOnArticle(@Param("marketCommentOnArticleDto") MarketCommentOnArticleDto marketCommentOnArticleDto);
+    public MarketArticleDto selectMarketCommentOnArticle(@Param("articleId") Integer articleId);
+    public void updateMarketCommentOnArticle(@Param("marketCommentOnArticleDto") MarketCommentOnArticleDto marketCommentOnArticleDto);
+    public void deleteMarketCommentOnArticle(@Param("id") Integer id);
+    
+    public void insertMarketProductInterestedLog(@Param("marketProductInterestedLLogDto") MarketProductInterestedLogDto marketProductInterestedLogDto);
+    public MarketProductInterestedLogDto selectMarketProductInterestedLogWhenUserInfo(@Param("marketUserId") Integer marketUserId);
+    public MarketProductInterestedLogDto selectMarketProductInterestedLogWhenArticleInfo(@Param("specificArticleId") Integer specificArticleId);
+    public void deleteMarketProductInterestedLog(@Param("specificArticleId") Integer specificArticleId);
+    
+    public void insertMarketDealedLog(@Param("marketDealedLogDto") MarketDealedLogDto marketDealedLogDto);
+    public MarketDealedLogDto selectMarketDealedLogWhenBuyer(@Param("buyerId") Integer buyerId);
+    public MarketDealedLogDto selectMarketDealedLogWhenSeller(@Param("sellerId") Integer sellerId);
+    
+    public void insertMarketReviewToUser(@Param("marketReviewOnUserDto") MarketReviewOnUserDto marketReviewOnUserDto);
+    public MarketReviewOnUserDto selectMarketReviewToUser(@Param("evaluatedUserId") Integer evaluatedUserId);
+    public void updateMarketReviewToUser(@Param("marketReviewOnUserDto") MarketReviewOnUserDto marketReviewOnUserDto);
+    public void deleteMarketReviewToUser(@Param("evaluatedUserId") Integer evaluatedUserId);
+    
+    // 페이지에 따른 추가 crud
+    
+    public void selectMarketArticleByTitleSearchWord(@Param("searchWord") String searchWord);
+    public void selectMarketArticleByContentSearchWord(@Param("searchWord") String searchWord);
+    
 }
