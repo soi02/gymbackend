317ed36c8d0cf458601af768cc87942a5baa2560
yoonsoo <fromyoonsoo@gmail.com>
2025-08-18 05:11:18 +0900

    획득 노리개 로직 수정

--- src/main/java/com/ca/gymbackend/challenge/dto/ChallengeUserInfo.java
+++ src/main/java/com/ca/gymbackend/challenge/dto/ChallengeUserInfo.java
@@ -6,6 +6,8 @@ import lombok.Data;
 
 @Data
 public class ChallengeUserInfo {
+    private String challengeTitle; // 추가
+    private int totalPeriod; // 추가
     private LocalDateTime personalJoinDate;
     private LocalDateTime personalEndDate;
 }
