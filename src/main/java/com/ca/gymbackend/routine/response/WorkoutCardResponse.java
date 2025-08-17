bc80bd3bafdbd9d0b970372f1ccd2605a695cd3d
jinnycalm <jinnycalm@gmail.com>
2025-08-18 02:51:25 +0900

    일지수정

--- src/main/java/com/ca/gymbackend/routine/response/WorkoutCardResponse.java
+++ src/main/java/com/ca/gymbackend/routine/response/WorkoutCardResponse.java
@@ -4,15 +4,14 @@ import java.time.LocalDateTime;
 
 import lombok.Data;
 
-
 @Data
 public class WorkoutCardResponse {
     private int workoutId;
-    private int setCount;        // 세트 수
-    private int workoutCount;    // 운동(종목) 수 = distinct element_id
-    private int calories;        // 칼로리 (workout_log 기준)
-    private double totalVolume;  // Σ(kg * reps)
-        private String memo;        // ← 추가
-    private String pictureUrl;  // ← 추가
+    private int setCount; 
+    private int workoutCount;
+    private int calories;
+    private double totalVolume;
+    private String memo;
+    private String pictureUrl; 
     private LocalDateTime createdAt;
 }
